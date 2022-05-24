f42ce81b75b7ccf7451a5ada15cb09b726d6ead0
meiousei9 <cameronavvampato@gmail.com>
2022-05-24 11:32:02 -0400

    add books to myLibrary and user display implemented

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 javascript practice, library project
 
 https://www.theodinproject.com/lessons/node-path-javascript-library
+
+
+todo list:
+- new book button (hide form otherwise?)
+    - Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
+- basic css for site
+    - header
+    - footer
+    - card-display
+- css for new book
+    - appears when click "new book" on top of everythign else
+    - disappears if "cancel" or did add new book
+
+
+- Add a button on each book’s display to remove the book from the library. 
+    - You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
+- Add a button on each book’s display to change its read status. 
+    - To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
+- make cards draggable to change index in array / display
+    - https://www.youtube.com/watch?v=wv7pvH1O5Ho
+- store books in local
+- advanced css, maybe
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/*
+
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
@@ -7,3 +9,75 @@ function Book(title, author, pages, readStatus) {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
     }
 }
+
+*/
+
+let myLibrary = [];
+
+class Book{
+    constructor(title, author, pages, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    }
+    info(){
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
+    }
+}
+
+/*************/
+/* FUNCTIONS */
+/*************/
+
+//adds book to myLibrary array
+function addBookToLibrary(book) {
+    //add book to myLibrary
+    myLibrary.push(book);
+}
+
+function createBookCard(book) {
+    const cardDisplay = document.querySelector('.card-display');
+
+    const card = document.createElement('div');
+
+    card.innerHTML = `
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <p>${book.pages}</p>
+        <p>replace me test read status</p>
+    `;
+
+    cardDisplay.appendChild(card);
+}
+
+/***********/
+/* BUTTONS */
+/***********/
+//makes submit button do stuff
+document.querySelector('#newBookForm').addEventListener('submit', function(e) {
+    e.preventDefault(); //prevent page refresh
+
+    //creates book object
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+
+    let readStatus = null; //deals with radio
+    let readStatusAnswers = document.getElementsByName('readStatus'); 
+    for (let i = 0; i < readStatusAnswers.length; i++) {
+        if (readStatusAnswers[i].checked)
+            readStatus = readStatusAnswers[i];
+    }
+
+    let newBook = new Book(title, author, pages, readStatus);
+
+
+    addBookToLibrary(newBook); //add book to library
+
+    createBookCard(newBook); //creates Book display card, adds to screen
+
+
+    //addBookToLibrary(e);
+});
+
